275f1310795b14c1b6500160e4e0a5b18483a369
Tape <trid.skivi@gmail.com>
2022-06-19 17:47:54 +0300

    made a program for the second task

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -4,3 +4,22 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+Console.WriteLine("Please enter the first number");
+int first_num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter the second number");
+int second_num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter the third number");
+int third_num = Convert.ToInt32(Console.ReadLine());
+
+int max = first_num;
+
+if(max < second_num) {
+    max = second_num;
+}
+if (max < third_num) {
+     max = third_num;
+}
+
+Console.WriteLine("the maximum number entered is " + max);
